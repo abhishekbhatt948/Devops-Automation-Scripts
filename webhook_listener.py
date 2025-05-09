fed98baca833fe72dd1a488565f24134ec0804db
Abhishek Bhatt <Abhishekbhatt948@gmail.com>
2025-05-09 19:21:00 +0530

    update  webhook_listner.py

--- webhook_listener.py
+++ webhook_listener.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    subprocess.call(["/home/ec2-user/sync_repo.sh"])
+    subprocess.call(["/home/ubuntu/Devops-Automation-Scripts/sync_repo.sh"])
     return "Pulled", 200
 
 if __name__ == "__main__":
